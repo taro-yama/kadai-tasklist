d55197c5a1ccbc25445f81a5550a85ae32a5a564
taro.yamada <taroy0722@gmail.com>
2022-08-14 14:45:25 +0000

    CRUD tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;    // 追加 
+
+class TasksController extends Controller
+{
+    // getでtasks/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $tasks = Task::all();
+// dd($tasks[0]);
+        // メッセージ一覧ビューでそれを表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // getでtasks/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $message = new Task;
+
+        // メッセージ作成ビューを表示
+        return view('tasks.create', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    // postでtasks/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+// dd($request);
+        // メッセージを作成
+        $message = new Task;
+        $message->content = $request->content;
+        $message->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // getでtasks/idにアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        // idの値でメッセージを検索して取得
+        $message = Task::findOrFail($id);
+
+        // メッセージ詳細ビューでそれを表示
+        return view('tasks.show', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // getでtasks/id/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        // idの値でメッセージを検索して取得
+        $message = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        // idの値でメッセージを検索して取得
+        $message = Task::findOrFail($id);
+        // メッセージを更新
+        $message->content = $request->content;
+        $message->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // deleteでtasks/idにアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        // idの値でメッセージを検索して取得
+        $message = Task::findOrFail($id);
+        // メッセージを削除
+        $message->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
